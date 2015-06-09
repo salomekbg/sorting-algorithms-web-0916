5e12ce2e11afa8132c66c16d5793c34b42337bed
changamanda <amanda.chang10@gmail.com>
2015-06-09 13:38:19 -0400

    Solution

--- sort.rb
+++ sort.rb
@@ -1,2 +1,15 @@
-def your_sort
+def your_sort(array, &block)
+  normalized_block = block || Proc.new{|a, b| a <=> b}
+  sort(array, normalized_block)   
+end
+
+def sort(array, block)
+  array.inject([]) { |sorted, element| insert_in_right_place(sorted, element, block) }
+end
+
+def insert_in_right_place(sorted_list, element, block)
+  sorted_list.each_with_index do |list_el, i|
+    return sorted_list.insert(i, element) if block.call(element, list_el) < 1
+  end
+  sorted_list << element
 end
