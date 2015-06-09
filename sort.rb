33d21564f853540c08f3179cf39a82feb86d1831
changamanda <amanda.chang10@gmail.com>
2015-06-09 16:11:38 -0400

    Remove middle helper

--- sort.rb
+++ sort.rb
@@ -1,10 +1,6 @@
 def your_sort(array, &block)
   normalized_block = block || Proc.new{|a, b| a <=> b}
-  sort(array, normalized_block)   
-end
-
-def sort(array, block)
-  array.inject([]) { |sorted, element| insert_in_right_place(sorted, element, block) }
+  array.inject([]) { |sorted, element| insert_in_right_place(sorted, element, normalized_block) }
 end
 
 def insert_in_right_place(sorted_list, element, block)
